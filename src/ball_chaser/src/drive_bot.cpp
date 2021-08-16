b54d8be48b33a906d0ccb89dfbb8ebcbc4913b56
AMHafez <53947645+AMHafez@users.noreply.github.com>
2021-08-17 01:34:29 +0200

    Update drive_bot.cpp

--- src/ball_chaser/src/drive_bot.cpp
+++ src/ball_chaser/src/drive_bot.cpp
@@ -44,7 +44,7 @@ int main(int argc, char** argv)
     motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
 
     // TODO: Define a drive /ball_chaser/command_robot service with a handle_drive_request callback function
- 	ros::ServiceServer service = n.advertiseService("/drive_bot/command_robot",
+ 	ros::ServiceServer service = n.advertiseService("/ball_chaser/command_robot",
 handle_drive_request);
 
     ROS_INFO("Ready to send drive commands");
